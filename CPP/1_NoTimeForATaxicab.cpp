7d0648d06f7b871e5831b8f721afb9a670efb2c1
Paripás Viktor <viktorlevente.paripas@afconsult.com>
2018-10-24 16:35:06 +0200

    Day 1, part 2 is wrong but same as python solution

--- CPP/1_NoTimeForATaxicab.cpp
+++ CPP/1_NoTimeForATaxicab.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <tuple>
 #include <fstream>
+#include <set>
 using namespace std;
 
 // main() is where program execution begins.
@@ -12,9 +13,11 @@ class Bunny {
         void turn(char direction);
         void walk(int steps);
         int getDistance(void);
-        tuple<int,int> getCoordinates(void);
+        pair<int,int> getCoordinates(void);
+        void storePosition(void);
         int drctn;
         int x, y;
+        set<pair<int, int>> visited;
 };
 Bunny::Bunny(int direction) {
     drctn = direction;
@@ -51,31 +54,51 @@ void Bunny::walk(int steps) {
 int Bunny::getDistance(void) {
     return abs(x) + abs(y);
 }
-tuple<int,int> Bunny::getCoordinates(void) {
-    return make_tuple(x, y);
+pair<int,int> Bunny::getCoordinates(void) {
+    return make_pair(x, y);
+}
+void Bunny::storePosition(void) {
+    visited.insert(make_pair(x, y));
 }
 
 int main() {
     Bunny bunny(0);
-    ifstream infile("1_NoTimeForATaxicab.txt"); // opens file
     string line = "";
-    getline(infile, line, '#');                 // data stored in line
-    size_t pos = 0;
+     size_t pos = 0;
     string instr;
-    while ((pos = line.find(", ")) != string::npos) {   // until there are delimiters left
-        instr = line.substr(0, pos);            // read string before delimiter (current instruction)
-        line.erase(0, pos + 2);                 // erase instruction from string
+    
+    ifstream infile("1_NoTimeForATaxicab.txt");             // opens file
+    getline(infile, line, '#');                             // data stored in line
+    
+    bool foundHQ = false;
+    
+    while ((pos = line.find(", ")) != string::npos) {       // until there are delimiters left
+        instr = line.substr(0, pos);                        // read string before delimiter (current instruction)
+        line.erase(0, pos + 2);                             // erase instruction from string
         
-        char drctn = instr.at(0);               // drctn = instr[0]
+        char drctn = instr.at(0);                           // drctn = instr[0]
         int steps = stoi(instr.substr(1, instr.length()));  // steps = instr[1:]
-        bunny.turn(drctn);
-        bunny.walk(steps);
-    }                                           // one more for the last instruction
+        bunny.turn(drctn);                                  // bunny turns drctn
+        bunny.walk(steps);                                  // bunny walks steps
+        
+        if(!foundHQ) {                                      // if HQ not found yet
+            pair<int,int> coords = bunny.getCoordinates();
+            const bool is_in = bunny.visited.find(coords) != bunny.visited.end();
+            if(!is_in) {                                    // and if position not already visited
+                bunny.storePosition();
+            }
+            else {                                          // if position already visited
+                cout << bunny.getDistance() << endl;        // display solution to Part2 (wrong)
+                foundHQ = true;
+            }
+            bunny.storePosition();
+        }
+    }                                                       // one more for the last instruction
     char drctn = line.at(0);
     int steps = stoi(line.substr(1, line.length()));
     bunny.turn(drctn);
     bunny.walk(steps);
     
-    cout << bunny.getDistance() << endl;
+    cout << bunny.getDistance() << endl;                    // solution to Part1
     return 0;
 };
