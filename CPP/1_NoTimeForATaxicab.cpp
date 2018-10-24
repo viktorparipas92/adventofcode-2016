db1d9b051f1a0a24bbfc4a449e10395307b01a47
Paripás Viktor <viktorlevente.paripas@afconsult.com>
2018-10-24 10:41:44 +0200

    initial commit test

---
+++ CPP/1_NoTimeForATaxicab.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+// main() is where program execution begins.
+int main() {
+   std::cout << "Hello World" << std::endl; // prints Hello World
+   return 0;
+}
