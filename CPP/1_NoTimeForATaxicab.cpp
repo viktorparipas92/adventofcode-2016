5ff20f94b20870bb6d9e2d0f3c58b5da025d1087
Paripás Viktor <viktorlevente.paripas@afconsult.com>
2018-10-24 10:49:19 +0200

    .o file deleted

--- CPP/1_NoTimeForATaxicab.cpp
+++ CPP/1_NoTimeForATaxicab.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 // main() is where program execution begins.
+class bunny {
+	int x, y;
+};
+
 int main() {
    std::cout << "Hello World" << std::endl; // prints Hello World
    return 0;
